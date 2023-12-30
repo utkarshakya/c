d151c012a0a50efdfa6d65d60071b06c841156b1
Utkarsh Shakya <subhashshakya136@gmail.com>
2023-12-30 17:50:57 +0530

    Some unwanted files and folder are removed

--- someOtherConcepts/macros.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#define pi 3.14 // this is a macro
-#define area(r) (pi*r*r) // this is macro function
-
-int main(){
-    printf("Value of pi = %f\n", pi);
-    printf("Area of circle with radius 4 cm = %f", area(4));
-    return 0;
-}
